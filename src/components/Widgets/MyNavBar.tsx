3a7ee5324e3935e091a32399db720d3023d53d57
Rosario <marcodibartolo2@gmail.com>
2024-11-21 17:12:01 +0100

    Adding buste-paga link

--- src/components/Widgets/MyNavBar.tsx
+++ src/components/Widgets/MyNavBar.tsx
@@ -1,25 +1,11 @@
 import React from "react";
 
-const NavField = () => (
-  <span className="p-3  rounded-sm  font-semibold overflow-none text-nowrap text-ellipsis">
-    Funzionalità principali
-  </span>
-);
-
 function MyNavBar() {
   return (
     <nav className="w-full flex items-center p-3 shadow-md shadow-slate-300   overflow-x-auto justify-between">
-      <span className="flex  gap-3 ">
-        <NavField />
-        <NavField />
-        <NavField />
-      </span>
-      <span className="flex  gap-3 ">
-        <NavField />
-        <NavField />
-      </span>
-
-      <div className="flex"> </div>
+      <div className="flex">
+        <button  className=" hover:text-teal-500 active:bg-slate-100 rounded-xl p-3 transition-all active:scale-75" ><a  className = " font-normal" href="/buste-paga"> Estrazione buste paga.</a></button>
+      </div>
     </nav>
   );
 }
