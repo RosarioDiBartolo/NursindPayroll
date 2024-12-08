b6787c400c3dc780a8907a8d2a6e52ac371816f1
Rosario <marcodibartolo2@gmail.com>
2024-12-08 15:18:06 +0100

    fixing differenziale api call

--- src/components/Widgets/Block.tsx
+++ src/components/Widgets/Block.tsx
@@ -56,7 +56,7 @@ function Differenziale({ file }: BlockProps) {
     const formData = new FormData();
     formData.append("file", file);
      
-     const res =  await apiClient.post(`/api/differenziale/Policlinico`, formData, {  })
+     const res =  await apiClient.post(`/api/differenziale`, formData, {  })
       return  await res.data
       
   };
